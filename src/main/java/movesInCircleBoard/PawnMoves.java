201decf4acd99e365df391918c59b58e3b162781
aczapata <andreaczapata@gmail.com>
2016-12-07 00:57:10 +0100

    Add pawn capture movements

--- src/main/java/movesInCircleBoard/PawnMoves.java
+++ src/main/java/movesInCircleBoard/PawnMoves.java
@@ -44,14 +44,48 @@ public class PawnMoves implements IMove {
 		}
 	}
 
+	public void captureMoveBeforeCenter(Piece piece, ArrayList<Square> list, int newX) {
+		int newY = piece.getSquare().getPozY() + 1;
+		if (!piece.pieceBehaviour.isout(newX, newY)) {
+			Square moveSq = piece.getChessboard().getSquares()[newX][newY];
+			if (moveSq.piece != null) {// check if can hit left
+				if (piece.getPlayer() != moveSq.piece.getPlayer() && !moveSq.piece.getName().equals("King")) {
+					if (piece.myKing().willBeSafeWhenMoveOtherPiece(piece.getSquare(), moveSq)) {
+						list.add(moveSq);
+					}
+				}
+			}
+		}
+
+	}
+
+	public void captureMoveAfterCenter(Piece piece, ArrayList<Square> list, int newX) {
+		int newY = piece.getSquare().getPozY() - 1;
+		if (!piece.pieceBehaviour.isout(newX, newY)) {
+			Square moveSq = piece.getChessboard().getSquares()[newX][newY];
+
+			if (moveSq.piece != null) {// check if can hit left
+				if (piece.getPlayer() != moveSq.piece.getPlayer() && !moveSq.piece.getName().equals("King")) {
+					if (piece.myKing().willBeSafeWhenMoveOtherPiece(piece.getSquare(), moveSq)) {
+						list.add(moveSq);
+					}
+				}
+			}
+		}
+	}
+
 	public ArrayList<Square> getMoves(Piece piece1) {
 		Pawn piece = (Pawn) piece1;
 		ArrayList<Square> list = new ArrayList<>();
 		if (piece.getSquare().getPozY() < 5) {
 			if (!piece.passedCenter) {
 				regularMoveBeforeCenter(piece, list);
+				captureMoveBeforeCenter(piece, list, piece.getSquare().getPozX() + 1);
+				captureMoveBeforeCenter(piece, list, piece.getSquare().getPozX() - 1);
 			} else {
 				regularMoveAfterCenter(piece, list);
+				captureMoveAfterCenter(piece, list, piece.getSquare().getPozX() + 1);
+				captureMoveAfterCenter(piece, list, piece.getSquare().getPozX() - 1);
 			}
 		} else if (piece.getSquare().getPozY() == 5) {
 			if (!piece.passedCenter) {
@@ -59,6 +93,8 @@ public class PawnMoves implements IMove {
 			} else {
 				passCenter(piece, list);
 				regularMoveAfterCenter(piece, list);
+				captureMoveAfterCenter(piece, list, piece.getSquare().getPozX() + 1);
+				captureMoveAfterCenter(piece, list, piece.getSquare().getPozX() - 1);
 			}
 
 		}
