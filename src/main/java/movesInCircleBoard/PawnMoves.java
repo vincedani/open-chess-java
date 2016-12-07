d65787dd8781431a77b2bc0f005dcec632885e4d
aczapata <andreaczapata@gmail.com>
2016-12-07 01:10:14 +0100

    Add border capture movements for pawns

--- src/main/java/movesInCircleBoard/PawnMoves.java
+++ src/main/java/movesInCircleBoard/PawnMoves.java
@@ -44,8 +44,8 @@ public class PawnMoves implements IMove {
 		}
 	}
 
-	public void captureMoveBeforeCenter(Piece piece, ArrayList<Square> list, int newX) {
-		int newY = piece.getSquare().getPozY() + 1;
+	public void captureMove(Piece piece, ArrayList<Square> list, int newX, int newY) {
+
 		if (!piece.pieceBehaviour.isout(newX, newY)) {
 			Square moveSq = piece.getChessboard().getSquares()[newX][newY];
 			if (moveSq.piece != null) {// check if can hit left
@@ -57,35 +57,20 @@ public class PawnMoves implements IMove {
 			}
 		}
 
-	}
-
-	public void captureMoveAfterCenter(Piece piece, ArrayList<Square> list, int newX) {
-		int newY = piece.getSquare().getPozY() - 1;
-		if (!piece.pieceBehaviour.isout(newX, newY)) {
-			Square moveSq = piece.getChessboard().getSquares()[newX][newY];
-
-			if (moveSq.piece != null) {// check if can hit left
-				if (piece.getPlayer() != moveSq.piece.getPlayer() && !moveSq.piece.getName().equals("King")) {
-					if (piece.myKing().willBeSafeWhenMoveOtherPiece(piece.getSquare(), moveSq)) {
-						list.add(moveSq);
-					}
-				}
-			}
-		}
 	}
 
 	public ArrayList<Square> getMoves(Piece piece1) {
 		Pawn piece = (Pawn) piece1;
 		ArrayList<Square> list = new ArrayList<>();
+		int x = piece.getSquare().getPozX(), y = piece.getSquare().getPozY();
+
 		if (piece.getSquare().getPozY() < 5) {
 			if (!piece.passedCenter) {
 				regularMoveBeforeCenter(piece, list);
-				captureMoveBeforeCenter(piece, list, piece.getSquare().getPozX() + 1);
-				captureMoveBeforeCenter(piece, list, piece.getSquare().getPozX() - 1);
+				captureMovesBeforeCenter(piece, list);
 			} else {
 				regularMoveAfterCenter(piece, list);
-				captureMoveAfterCenter(piece, list, piece.getSquare().getPozX() + 1);
-				captureMoveAfterCenter(piece, list, piece.getSquare().getPozX() - 1);
+				captureMovesAfterCenter(piece, list);
 			}
 		} else if (piece.getSquare().getPozY() == 5) {
 			if (!piece.passedCenter) {
@@ -93,12 +78,41 @@ public class PawnMoves implements IMove {
 			} else {
 				passCenter(piece, list);
 				regularMoveAfterCenter(piece, list);
-				captureMoveAfterCenter(piece, list, piece.getSquare().getPozX() + 1);
-				captureMoveAfterCenter(piece, list, piece.getSquare().getPozX() - 1);
+				captureMovesAfterCenter(piece, list);
 			}
 
 		}
 
 		return list;
+	}
+
+	private void captureMovesBeforeCenter(Pawn piece, ArrayList<Square> list) {
+		int x = piece.getSquare().getPozX(), y = piece.getSquare().getPozY();
+		if (x == 0) {
+			captureMove(piece, list, x + 1, y + 1);
+			captureMove(piece, list, 23, y + 1);
+
+		} else if (x == 23) {
+			captureMove(piece, list, 0, y + 1);
+			captureMove(piece, list, x - 1, y + 1);
+		} else {
+			captureMove(piece, list, x + 1, y + 1);
+			captureMove(piece, list, x - 1, y + 1);
+		}
+	}
+	
+	private void captureMovesAfterCenter(Pawn piece, ArrayList<Square> list) {
+		int x = piece.getSquare().getPozX(), y = piece.getSquare().getPozY();
+		if (x == 0) {
+			captureMove(piece, list, x + 1, y - 1);
+			captureMove(piece, list, 23, y - 1);
+
+		} else if (x == 23) {
+			captureMove(piece, list, 0, y - 1);
+			captureMove(piece, list, x - 1, y - 1);
+		} else {
+			captureMove(piece, list, x + 1, y - 1);
+			captureMove(piece, list, x - 1, y - 1);
+		}
 	}
 }
