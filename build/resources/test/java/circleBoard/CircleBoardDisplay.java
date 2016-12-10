9b7328996f9ac462c88357b9db81d72755046f77
Jose R. Cadena <joroca09@hotmail.com>
2016-12-10 05:52:21 +0100

    Cleaned build.gradle file

---
+++ build/resources/test/java/circleBoard/CircleBoardDisplay.java
@@ -0,0 +1,246 @@
+package main.java.circleBoard;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import main.java.board.ChessboardDisplay;
+import main.java.board.ChessboardLayout;
+import main.java.board.Square;
+
+public class CircleBoardDisplay extends ChessboardDisplay {
+
+	public Square activeSquare;
+	public Image upDownLabel;
+	public Image LeftRightLabel;
+	public Point topLeftPoint;
+	public int active_x_square;
+	public int active_y_square;
+	
+	public static final int img_x = 5;//image x position (used in JChessView class!)
+    public static final int img_y = img_x;//image y position (used in JChessView class!)
+    public static final int img_widht = 480;//image width
+    public static final int img_height = img_widht;//image height
+    
+	private ChessboardLayout board_layout;
+	boolean renderLabels, upsideDown;
+	Square[][] squares;
+	CircleBoard board;
+	
+	public CircleBoardDisplay(Image upDownLabel, Image leftRightLabel, Point topLeft, boolean renderLabels, boolean upsideDown, CircleBoard board) {
+		this.upDownLabel = upDownLabel;
+		LeftRightLabel = leftRightLabel;
+		this.topLeftPoint = topLeft;
+		this.board_layout=board.board_layout;
+		this.renderLabels= renderLabels;
+		this.upsideDown= upsideDown;
+		this.squares= board.initial.getSquares();
+		this.board= board;
+		activeSquare = null;
+		active_x_square = -1;
+        active_y_square = -1;
+        
+        this.setDoubleBuffered(true);
+        drawLabels(get_square_height());
+	}
+	
+	
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		if (renderLabels) {
+			if (topLeftPoint.x <= 0 && topLeftPoint.y <= 0) {
+				drawLabels(get_square_height());
+			}
+
+			g2d.drawImage(this.upDownLabel, 0, 0, null);
+			g2d.drawImage(this.upDownLabel, 0, board_layout.image.getHeight(null) + topLeftPoint.y, null);
+			g2d.drawImage(this.LeftRightLabel, 0, 0, null);
+			g2d.drawImage(this.LeftRightLabel, board_layout.image.getHeight(null) + topLeftPoint.x, 0, null);
+		}
+		g2d.drawImage(board_layout.image, topLeftPoint.x, topLeftPoint.y, null);
+		drawPieces(g);
+		drawHighlightedSquares(g2d);
+		
+	}/*--endOf-paint--*/ 
+	
+
+	private void drawPieces(Graphics g) {
+		for (int i = 0; i < 24; i++) // drawPiecesOnSquares
+		{
+			for (int y = 0; y < 6; y++) {
+				if (squares[i][y].piece != null) {
+					squares[i][y].piece.draw(g);// draw image of Piece
+				}
+			}
+		} // --endOf--drawPiecesOnSquares
+	}
+
+	public Point indexToCartesian(Point p1){
+		int xi = p1.x;
+		int yi = p1.y;
+		
+		int ri = (int) (getRadius() - (yi + 1) * get_square_height());
+		int rs = (int) (ri + get_square_height());
+
+		int a1 = (6 - xi) * 15;
+		int a2 = a1 - 15;
+		int rm = (rs + ri) / 2;
+		int am = (a1 + a2) / 2;
+
+		int xm = (int) (topLeftPoint.x + getRadius() + (int) (rm * Math.cos(Math.toRadians(am))) - get_square_height() / 2);
+		int ym = (int) (topLeftPoint.y + getRadius() - (int) (rm * Math.sin(Math.toRadians(am))) - get_square_height() / 2);
+		
+		return new Point(xm, ym);
+
+	}
+	
+	public void drawHighlightedSquares(Graphics2D g2d) {
+
+		if (activeSquare != null) {
+			int xi = activeSquare.getPozX();
+			int yi = activeSquare.getPozY();
+			
+			Point pm = indexToCartesian(new Point(xi, yi));
+
+			int xm = pm.x;
+			int ym = pm.y;
+			
+			Image tempImage = board_layout.orgSelSquare;
+			BufferedImage resized = resizeImage(tempImage, get_square_height());
+			board_layout.selSquare = resized.getScaledInstance(get_square_height(), get_square_height(), 0);
+			g2d.drawImage(board_layout.selSquare, xm, ym, null);
+			
+			if (activeSquare.piece != null)
+            {
+                ArrayList<Square> moves = activeSquare.piece.allMoves(false);
+                for (Square sq : moves)
+                {
+                   Point p_sq = indexToCartesian(new Point(sq.getPozX(),sq.getPozY()));
+                    
+                    tempImage = board_layout.orgAbleSquare;
+                    resized = resizeImage(tempImage,get_square_height());
+        			board_layout.ableSquare = resized.getScaledInstance(get_square_height(), get_square_height(), 0);
+        			
+                    g2d.drawImage(board_layout.ableSquare, p_sq.x, p_sq.y, null);
+                }
+            }
+
+		}
+	}
+
+
+	private BufferedImage resizeImage(Image tempImage, int height) {
+		BufferedImage resized = new BufferedImage(height, height, BufferedImage.TYPE_INT_ARGB_PRE);
+		Graphics2D imageGr = (Graphics2D) resized.createGraphics();
+		imageGr.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		imageGr.drawImage(tempImage, 0, 0, height, height, null);
+		imageGr.dispose();
+		return resized;
+	}
+
+	public void resizeChessboard(int height) {
+		BufferedImage resized = resizeImage(board_layout.orgImage, height);
+		board_layout.image = resized.getScaledInstance(height, height, 0);
+		
+		int square_height = (height - height / 3) / 6;
+
+		if (renderLabels) {
+			height += 2 * (this.upDownLabel.getHeight(null));
+		}
+		
+		this.setSize(height, height);
+		this.drawLabels(square_height);
+	}
+
+	protected final void drawLabels(float square_height2) {
+		// BufferedImage uDL = new BufferedImage(800, 800,
+		// BufferedImage.TYPE_3BYTE_BGR);
+		int min_label_height = 20;
+		int labelHeight = (int) Math.ceil(square_height2 / 4);
+		labelHeight = (labelHeight < min_label_height) ? min_label_height : labelHeight;
+		int labelWidth = (int) Math.ceil(square_height2 * 8 + (2 * labelHeight));
+		BufferedImage uDL = new BufferedImage(labelWidth + min_label_height, labelHeight, BufferedImage.TYPE_3BYTE_BGR);
+		Graphics2D uDL2D = (Graphics2D) uDL.createGraphics();
+		uDL2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		uDL2D.setColor(Color.white);
+
+		uDL2D.fillRect(0, 0, labelWidth + min_label_height, labelHeight);
+		uDL2D.setColor(Color.black);
+		uDL2D.setFont(new Font("Arial", Font.BOLD, 12));
+		int addX = (int) (square_height2 / 2);
+		if (renderLabels) {
+			addX += labelHeight;
+		}
+
+		String[] letters = { "a", "b", "c", "d", "e", "f", "g", "h" };
+
+		int j = 1;
+		for (int i = letters.length; i > 0; i--, j++) {
+			uDL2D.drawString(letters[i - 1], (square_height2 * (j - 1)) + addX, 10 + (labelHeight / 3));
+		}
+
+		uDL2D.dispose();
+		this.upDownLabel = uDL;
+
+		uDL = new BufferedImage(labelHeight, labelWidth + min_label_height, BufferedImage.TYPE_3BYTE_BGR);
+		uDL2D = (Graphics2D) uDL.createGraphics();
+		uDL2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		uDL2D.setColor(Color.white);
+		// uDL2D.fillRect(0, 0, 800, 800);
+		uDL2D.fillRect(0, 0, labelHeight, labelWidth + min_label_height);
+		uDL2D.setColor(Color.black);
+		uDL2D.setFont(new Font("Arial", Font.BOLD, 12));
+
+		j = 1;
+		for (int i = 8; i > 0; i--, j++) {
+			uDL2D.drawString(new Integer(i).toString(), 3 + (labelHeight / 3), (square_height2 * (j - 1)) + addX);
+		}
+
+		uDL2D.dispose();
+		this.LeftRightLabel = uDL;
+	}
+
+
+	public int getUpDownLabelHeight() {
+		return upDownLabel.getHeight(null);
+	}
+
+
+	public int getHeight()
+    {
+        return board_layout.image.getHeight(null);
+    }/*--endOf-get_height--*/
+
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int get_square_height() {
+		return (getRadius()-getRadius()/3)/6;
+	}
+
+	public int getRadius() {
+		return board_layout.image.getHeight(null)/2;
+	}
+
+	@Override
+	public Point getTopLeftPoint() {
+		if (renderLabels)
+        {
+            return new Point(topLeftPoint.x + upDownLabel.getHeight(null), topLeftPoint.y + upDownLabel.getHeight(null));
+        }
+		return topLeftPoint;
+	}
+
+}
---
+++ build/resources/test/java/pieces/PieceBehaviour.java
@@ -0,0 +1,127 @@
+package main.java.pieces;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import main.java.board.IChessboard;
+import main.java.board.Square;
+import main.java.circleBoard.CircleBoard;
+import main.java.game.Player;
+import main.java.squareBoard.SquareBoard;
+
+public class PieceBehaviour {
+
+	private IChessboard chessboard;
+
+	private Square square;
+	private Player player;
+	private String name;
+	protected String symbol;
+
+	public PieceBehaviour(IChessboard chessboard, Player player) {
+
+		this.setChessboard(chessboard);
+		this.setPlayer(player);
+		this.setName(this.getClass().getSimpleName());
+	}
+
+	boolean canMove(Square square, ArrayList<Square> allmoves) {
+		// throw new UnsupportedOperationException("Not supported yet.");
+		ArrayList<Square> moves = allmoves;
+		for (Iterator<Square> it = moves.iterator(); it.hasNext();) {
+			Square sq = it.next();// get next from iterator
+			if (sq == square) {// if address is the same
+				return true; // piece canMove
+			}
+		}
+		return false;// if not, piece cannot move
+	}
+
+	public boolean isout(int x, int y) {
+		if (chessboard instanceof SquareBoard) {
+			if (x < 0 || x > 7 || y < 0 || y > 7) {
+				return true;
+			}
+		} else if (chessboard instanceof CircleBoard) {
+			if (x < 0 || x > 23 || y < 0 || y > 5) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param x
+	 *            y position on chessboard
+	 * @param y
+	 *            y position on chessboard
+	 * @return true if can move, false otherwise
+	 */
+	public boolean checkPiece(int x, int y) {
+		if (getChessboard().getSquares()[x][y].piece != null
+				&& getChessboard().getSquares()[x][y].piece.getName().equals("King")) {
+			return false;
+		}
+		Piece piece = getChessboard().getSquares()[x][y].piece;
+		if (piece == null || // if this square is empty
+				piece.getPlayer() != this.getPlayer()) // or piece is another
+														// player
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Method check if piece has other owner than calling piece
+	 * 
+	 * @param x
+	 *            x position on chessboard
+	 * @param y
+	 *            y position on chessboard
+	 * @return true if owner(player) is different
+	 */
+	public boolean otherOwner(int x, int y) {
+		Square sq = getChessboard().getSquares()[x][y];
+		if (sq.piece == null) {
+			return false;
+		}
+		if (this.getPlayer() != sq.piece.getPlayer()) {
+			return true;
+		}
+		return false;
+	}
+
+	public Square getSquare() {
+		return square;
+	}
+
+	public void setSquare(Square square) {
+		this.square = square;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public IChessboard getChessboard() {
+		return chessboard;
+	}
+
+	public void setChessboard(IChessboard chessboard2) {
+		this.chessboard = chessboard2;
+	}
+
+}
